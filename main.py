ad79b642695ba6bcd56be7ce70af10de4068d56a
DilZam <dilyar.zamaliev@yandex.ru>
2026-04-02 11:50:48 +0300

    задача 5, 6

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import arcade
 from to_rec import spn_sizes
 from to_scale import scale_map
 from move_map import to_move
-from arcade.gui import UIManager, UIFlatButton
+from arcade.gui import UIManager, UIFlatButton, UIInputText, UILabel
 from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
 
 WINDOW_WIDTH = 1280
@@ -21,20 +21,32 @@ class GameView(arcade.Window):
         self.manager = UIManager()
         self.manager.enable()
         self.anchor_layout = UIAnchorLayout(x=-310, y=-135)
-        self.box_layout = UIBoxLayout(vertical=False, space_between=10)
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)
         self.setup_widgets()
         self.anchor_layout.add(self.box_layout)
         self.manager.add(self.anchor_layout)
+        self.points = ""
 
     def setup_widgets(self):
         flat_button = UIFlatButton(text="Сменить тему", width=600, height=20)
-        flat_button.on_click = lambda event: self.theme_change()  # Не только лямбду, конечно
+        flat_button.on_click = lambda event: self.theme_change()
         self.box_layout.add(flat_button)
+        box = UIBoxLayout(vertical=False)
+        self.input_text = UIInputText(width=400, height=30, text="Введите адрес объекта",
+                                      border_color=(62, 70, 121, 255),
+                                      text_color=(62, 70, 121, 255))
+        box.add(self.input_text)
+        flat_button1 = UIFlatButton(text="Искать", width=200, height=30)
+        flat_button1.on_click = lambda event: self.get_coords(self.input_text.text)
+        box.add(flat_button1)
+        self.box_layout.add(box)
+        self.label = UILabel(text="", font_size=14, width=600, align="left", text_color=(62, 70, 121, 255))
+        self.box_layout.add(self.label)
 
     def setup(self):
-        self.x, self.y, self.spn = self.get_coords("Казань")
+        self.x, self.y, self.spn = 0, 0, 0
         self.theme = ""
-        self.get_image()
+        self.get_coords("Казань")
 
     def theme_change(self):
         if self.theme == "": self.theme="&theme=dark"
@@ -61,7 +73,7 @@ class GameView(arcade.Window):
         server_address = 'https://static-maps.yandex.ru/v1?'
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
         ll_spn = f'll={self.x},{self.y}&spn={self.spn},{self.spn}{self.theme}'
-        map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+        map_request = f"{server_address}{ll_spn}{self.points}&apikey={api_key}"
         response = requests.get(map_request)
 
         if not response:
@@ -80,15 +92,16 @@ class GameView(arcade.Window):
         api_key = '8013b162-6b42-4997-9691-77b7074026e0'
         geocoder_request = f'{server_address}apikey={api_key}&geocode={town}&format=json'
         response = requests.get(geocoder_request)
-        if response:
+        try:
             json_response = response.json()
             toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-            return [float(n) for n in toponym["Point"]["pos"].split(" ")] + [spn_sizes(toponym["boundedBy"]["Envelope"])]
+            self.x, self.y, self.spn =  [float(n) for n in toponym["Point"]["pos"].split(" ")] + [spn_sizes(toponym["boundedBy"]["Envelope"])]
+            self.points = f"&pt={self.x},{self.y},pm2ntl"
+            self.label.text = ""
+            self.get_image()
 
-        else:
-            print("Ошибка выполнения запроса:")
-            print(geocoder_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+        except Exception:
+            self.label.text = "Ничего не найдено"
 
     def on_key_press(self, key, modifiers):
         spn, x, y = self.spn, self.x, self.y
@@ -104,6 +117,8 @@ class GameView(arcade.Window):
             self.x, self.y = to_move(1, 0, 0, 0, self.x, self.y)
         if key == arcade.key.LEFT:
             self.x, self.y = to_move(0, 1, 0, 0, self.x, self.y)
+        if key == arcade.key.ENTER:
+            self.get_coords(self.input_text.text)
         if (spn, x, y) != (self.spn, self.x, self.y):
             self.get_image()
 def main():
